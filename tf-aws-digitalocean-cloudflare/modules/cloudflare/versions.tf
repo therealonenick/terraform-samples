d2c0e9dba1318e1cafd729571bc9a1b3329e150a
Nick <nick@theonenick.com>
2020-12-02 21:46:24 -0500

    update to tf13 syntax

--- tf-aws-digitalocean-cloudflare/modules/cloudflare/versions.tf
+++ tf-aws-digitalocean-cloudflare/modules/cloudflare/versions.tf
@@ -1,3 +1,8 @@
 terraform {
-  required_version = "=0.12.29"
+  required_version = ">= 0.13"
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
 }
--- tf-aws-digitalocean-cloudflare/versions.tf
+++ tf-aws-digitalocean-cloudflare/versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = "=0.12.29"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
 }
